dd2335952ddcfdd0e4cd9948d032e22417ac6f06
Calvin Correli <calvin@correli.me>
2014-11-05 12:55:56 -0500

    Attempted fix for $.slidebars breakage (issue 135)
    I'm not sure how the API is supposed to work. I just do
    
    ```
    $.slidebars(siteClose: false)
    ```
    
    in my code.
    
    And with the original code, after this call, $.slidebars now refers to that object, not to the original $.slidebars function.
    
    Maybe this fix does what you intend?

--- development/slidebars.js
+++ development/slidebars.js
@@ -266,7 +266,7 @@
 		// ---------
 		// 007 - API
 		
-		this.slidebars = {
+		var slidebars = {
 			open: open, // Maps user variable name to the open method.
 			close: close, // Maps user variable name to the close method.
 			toggle: toggle, // Maps user variable name to the toggle method.
