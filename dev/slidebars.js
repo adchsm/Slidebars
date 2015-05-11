76097e6807aec2b147d3418151ccfd2190ad4fff
Adam Smith <adchsm@me.com>
2015-05-11 11:45:09 +0200

    Updated dev JS
    Loops through each of the off canvas elements adding them to a object.

--- dev/slidebars.js
+++ dev/slidebars.js
@@ -8,3 +8,19 @@
  * License url: http://opensource.org/licenses/MIT
  */
 
+var slidebars = function () {
+	var offCanvas = {};
+	
+	$( '[off-canvas]' ).each( function () {
+		var values = $( this ).attr( 'off-canvas' ).split( ' ', 3 );
+		
+		offCanvas[ values[0] ] = {
+			'id': values[0],
+			'side': values[1],
+			'style': values[2],
+			'element': $( this )
+		};
+	} );
+	
+	console.log( offCanvas );
+};
