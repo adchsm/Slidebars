884f3562507d384de01fa5a59a74e2bc1ccb9ce3
Adam Smith <adchsm@me.com>
2015-05-14 12:05:08 +0200

    Updated dev version
    Added a load of security checks and active method

--- dev/slidebars.js
+++ dev/slidebars.js
@@ -17,6 +17,15 @@ var slidebars = function () {
 	slidebars = this;
 	
 	/**
+	 * Accepted Parameters
+	 */
+	
+	slidebars.parameters = {
+		'side': [ 'top', 'right', 'bottom', 'left' ],
+		'style': [ 'reveal', 'push', 'overlay', 'shift' ]
+	};
+	
+	/**
 	 * Instances
 	 */
 	
@@ -27,17 +36,29 @@ var slidebars = function () {
 	 */
 	
 	slidebars.init = function () {
-		// Loop through and setup each Slidebar
+		// Loop through and register each Slidebar
 		$( '[off-canvas]' ).each( function () {
-			var values = $( this ).attr( 'off-canvas' ).split( ' ', 3 );
+			// Get the Slidebar parameters
+			var parameters = $( this ).attr( 'off-canvas' ).split( ' ', 3 );
 			
-			slidebars.instances[ values[0] ] = {
-				'id': values[0],
-				'side': values[1],
-				'style': values[2],
-				'element': $( this ),
-				'active': false
-			};
+			// Make sure a valid id, side and style are specified
+			if ( typeof parameters[0] !== 'undefined' && slidebars.parameters.side.indexOf( parameters[1] ) !== -1 && slidebars.parameters.style.indexOf( parameters[2] ) !== -1 ) {
+				// Check to see if the Slidebar exists
+				if ( ! ( parameters[0] in slidebars.instances ) ) {
+					// Register the Slidebar
+					slidebars.instances[ parameters[0] ] = {
+						'id': parameters[0],
+						'side': parameters[1],
+						'style': parameters[2],
+						'element': $( this ),
+						'active': false
+					};
+				} else {
+					throw "Error attempting to register a Slidebar, a Slidebar with ID '" + parameters[0] + "' already exists.";
+				}
+			} else {
+				throw "Error attempting to register a Slidebar, please specifiy a valid space separated 'id side style'.";
+			}
 		} );
 		
 		// Check canvas container height (test for vh support)
@@ -50,7 +71,7 @@ var slidebars = function () {
 	 * Animation
 	 */
 	
-	slidebars.animate = function ( id, amount ) {
+	slidebars.animate = function ( id ) {
 	};
 	
 	/**
@@ -64,6 +85,19 @@ var slidebars = function () {
 	};
 	
 	slidebars.toggle = function ( id ) {
+		// Check to see if the Slidebar exists
+		if ( id in slidebars.instances ) {
+			// Check its status
+			if ( slidebars.instances[ id ].active ) {
+				// It's open, close it
+				slidebars.close( id );
+			} else {
+				// It's closed, open it
+				slidebars.open( id );
+			}
+		} else {
+			throw "Error trying to toggle Slidebar, there is no Slidebar with ID '" + id + "'.";
+		}
 	};
 	
 	/**
@@ -71,36 +105,58 @@ var slidebars = function () {
 	 */
 	 
 	slidebars.active = function ( id ) {
+		// Check to see if the Slidebar exists
+		if ( id in slidebars.instances ) {
+			// Return it's status
+			return slidebars.instances[ id ].active;
+		} else {
+			throw "Error retrieving status of Slidebar, there is no Slidebar with ID '" + id + "'.";
+		}
 	};
 	
 	/**
-	 * Creation
+	 * Manage
 	 */
 	
 	slidebars.create = function ( id, side, style, content ) {
-		// Create new element
-		$( '<div id="' + id + '" off-canvas="' + id + ' ' + side + ' ' + style + '"></div>' ).appendTo( 'body' );
-		
-		// Add content to the Slidebar
-		if ( typeof content !== 'undefined' ) {
-			$( '#' + id ).html( content );
+		// Make sure a valid id, side and style are specified
+		if ( typeof id !== 'undefined' && slidebars.parameters.side.indexOf( side ) !== -1 && slidebars.parameters.style.indexOf( style ) !== -1 ) {
+			// Check to see if the Slidebar exists
+			if ( ! ( id in slidebars.instances ) ) {
+				// Create new element
+				$( '<div id="' + id + '" off-canvas="' + id + ' ' + side + ' ' + style + '"></div>' ).appendTo( 'body' );
+				
+				// Add content to the Slidebar
+				if ( typeof content !== 'undefined' ) {
+					$( '#' + id ).html( content );
+				}
+				
+				// Register the Slidebar
+				slidebars.instances[ id ] = {
+					'id': id,
+					'side': side,
+					'style': style,
+					'element': $( '#' + id ),
+					'active': false
+				};
+			} else {
+				throw "Error attempting to create a Slidebar, a Slidebar with ID '" + id + "' already exists.";
+			}
+		} else {
+			throw "Error attempting to create a Slidebar, please specifiy a valid space separated 'id side style'.";
 		}
-		
-		// Add Slidebar to instances
-		slidebars.instances[ id ] = {
-			'id': id,
-			'side': side,
-			'style': style,
-			'element': $( '#' + id ),
-			'active': false
-		};
 	};
 	
 	slidebars.destroy = function ( id ) {
-		// Remove the element
-		slidebars.instances[ id ].element.remove();
-		
-		// Remove Slidebar from instances
-		delete slidebars.instances[ id ];
+		// Check to see if the Slidebar exists
+		if ( id in slidebars.instances ) {
+			// Remove the element
+			slidebars.instances[ id ].element.remove();
+			
+			// Remove Slidebar from instances
+			delete slidebars.instances[ id ];
+		} else {
+			throw "Error trying to destroy Slidebar, there is no Slidebar with ID '" + id + "'.";
+		}
 	};
 };
